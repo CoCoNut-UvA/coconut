b17582e66e1a166f80ffe61af020288cf9f8bd35
Rick Teuthof <rmt1998@hotmail.nl>
2020-04-14 18:04:39 +0200

    Changed macros in gen-trav

--- cocogen/filegen/gen-trav.c
+++ cocogen/filegen/gen-trav.c
@@ -12,14 +12,17 @@
 #include "filegen/gen-util.h"
 #include "filegen/genmacros.h"
 
+static int indent = 0;
+
 void gen_trav_functions(Config *config, FILE *fp, Traversal *trav,
                         bool is_header) {
-    out("// Traversal functions for %s\n", trav->id);
+    out_comment("Traversal functions for %s", trav->id);
     char *travlwr = strlwr(trav->id);
     for (int i = 0; i < array_size(config->nodes); i++) {
         Node *node = array_get(config->nodes, i);
         char *nodelwr = strlwr(node->id);
-        out("Node *%s_%s(Node *arg_node, Info *arg_info);\n", travlwr, nodelwr);
+        out_field("Node *%s_%s(Node *arg_node, Info *arg_info)", travlwr,
+                  nodelwr);
         free(nodelwr);
     }
     out("\n");
@@ -47,30 +50,30 @@ void gen_trav_header(Config *config, FILE *fp) {
  * method is probably not optimal, TODO: find a better method.
  */
 void gen_traverse_children(Config *config, FILE *fp) {
-    out("Node *traverse_children(Node *arg_node, Info *arg_info) {\n");
-    out("    switch (NODE_TYPE(arg_node)) {\n");
+    out_start_func("Node *traverse_children(Node *arg_node, Info *arg_info)");
+    out_begin_switch("NODE_TYPE(arg_node)");
     for (int i = 0; i < array_size(config->nodes); i++) {
         Node *node = array_get(config->nodes, i);
         char *nodelwr = strlwr(node->id);
         char *nodeupr = strupr(node->id);
-        out("    case NT_%s:\n", nodelwr);
+        out_begin_case("NT_%s", nodelwr);
         for (int i = 0; i < array_size(node->children); i++) {
             Child *child = array_get(node->children, i);
             char *childupr = strupr(child->id);
-            out("        TRAV(%s_%s(arg_node), arg_info);\n", nodeupr,
-                childupr);
-
+            out_field("TRAV(%s_%s(arg_node), arg_info)", nodeupr, childupr);
             free(childupr);
         }
-        out("        break;\n");
+        out_field("break");
         free(nodelwr);
         free(nodeupr);
+        out_end_case();
     }
-    out("    default:\n");
+    out_begin_default_case();
     /// TODO: Do some error handling here
-    out("        break;\n");
-    out("    }\n");
-    out("}\n\n");
+    out_field("break");
+    out_end_case();
+    out_end_switch();
+    out_end_func();
 }
 
 void gen_trav_src(Config *config, FILE *fp) {
