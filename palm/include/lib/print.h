f6e424f5225fa926878ad16ef905cece241ab223
Rick Teuthof <rmt1998@hotmail.nl>
2020-04-21 10:05:08 +0200

    Revert "Changed includes"
    This reverts commit ccaf6580751e0397fddbc30797fe266b33deac58.

--- palm/include/lib/print.h
+++ palm/include/lib/print.h
@@ -3,8 +3,8 @@
 #include <stdio.h>
 #include <unistd.h>
 
-#include "array.h"
-#include "imap.h"
+#include "lib/array.h"
+#include "lib/imap.h"
 
 void print_char_n_times(char, size_t n);
 
@@ -27,13 +27,13 @@ enum PrintType { PT_error, PT_warning, PT_note };
 
 void print_user_error(char *header, const char *format, ...);
 
-void print_init_compilation_messages(char *header, char *filename, array *lines,
-                                     imap_t *parser_locations);
+void print_init_compilation_messages(char *header, char *filename,
+                                     array *lines, imap_t *parser_locations);
 
 void print_error(void *loc_obj, char *format, ...);
 void print_error_at(int lineno, int column, char *line, char *format, ...);
-void print_error_range(int lineno, int column_start, int column_end, char *line,
-                       char *format, ...);
+void print_error_range(int lineno, int column_start, int column_end,
+                       char *line, char *format, ...);
 
 void print_error_no_loc(char *format, ...);
 void print_warning(void *loc_obj, char *format, ...);
@@ -51,5 +51,5 @@ void print_note_no_loc(char *format, ...);
 void _print_internal_error(const char *file, const char *func, int line,
                            const char *format, ...);
 
-#define print_internal_error(...)                                              \
+#define print_internal_error(...)                                             \
     _print_internal_error(__FILE__, __func__, __LINE__, __VA_ARGS__)
