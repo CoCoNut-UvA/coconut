1eb0e2477dd7ac2fa26c477058605c961238b58a
Rick Teuthof <rmt1998@hotmail.nl>
2020-05-09 11:05:43 +0200

    Changed name to dynamic backend

--- cocogen/filegen/util.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "ast/ast.h"
-#include <stdio.h>
-
-void print_indent_level(int indent_level, FILE *);
-void generate_node_header_includes(Config *, FILE *, Node *);
-
-#include "genmacros.h"
