7fc7ae3771eea0b8f2021e579cdeeedc0fa73731
DF <leegbestand@gmail.com>
2020-09-03 23:40:39 +0200

    Integrate globals and input file

---
+++ bootstrap/src/globals.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "palm/hash_table.h"
+
+struct globals {
+    char *filename;
+    htable_st *line_map; // Maps line numbers to lines, this maps owns the lines values!
+    char *gen_src_dir;
+    char *gen_hdr_dir;
+};
+
+extern struct globals globals;
--- bootstrap/src/main.c
+++ bootstrap/src/main.c
@@ -1,6 +1,17 @@
 #include "commandline.h"
+#include "ccngen/ast.h"
+#include "ccn/phase_driver.h"
+#include <stddef.h>
+#include "globals.h"
+
+struct globals globals;
 
 int main(int argc, char *argv[])
 {
     CLprocessArgs(argc, argv);
+    globals.line_map = HTnew_Int(25);
+    struct ccn_node *node = NULL;
+    node = CCNstart(node);
+
+    return 0;
 }
