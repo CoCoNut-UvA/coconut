06625e34524d1a0ebe9ac31092e488a2652ad7b2
DF <leegbestand@gmail.com>
2020-12-31 21:45:43 +0100

    Update coconut.cmake

--- coconut.cmake
+++ coconut.cmake
@@ -14,7 +14,7 @@ add_subdirectory(${COCONUT_ROOT_DIR}/palm ${CMAKE_CURRENT_BINARY_DIR}/palm)
 
 function(cocogen_do_generate DSL_FILE BACKEND)
     set_property(DIRECTORY APPEND PROPERTY CMAKE_CONFIGURE_DEPENDS "${DSL_FILE}")
-    set_property(DIRECTORY APPEND PROPERTY CMAKE_CONFIGURE_DEPENDS "${PROJECT_BINARY_DIR}/generated/src/action_handlers.c")
+    set_property(DIRECTORY APPEND PROPERTY CMAKE_CONFIGURE_DEPENDS "${CMAKE_BINARY_DIR}/generated/src/action_handlers.c")
     if (NOT EXISTS "${COCOGEN_DIR}/cocogen")
         message(FATAL_ERROR "Could not find the cocogen executable in path: ${COCOGEN_DIR}
 Maybe you forgot building the coconut project?")
@@ -23,8 +23,8 @@ Maybe you forgot building the coconut project?")
     message(STATUS "Generating with cocogen
     ") # Forces newline
     message("${COCOGEN_DIR}")
-    message("${PROJECT_BINARY_DIR}")
-    execute_process(COMMAND "${COCOGEN_DIR}/cocogen" "--backend" "${BACKEND}" "--gen-user-files" "${DSL_FILE}"
+    message("${CMAKE_BINARY_DIR}")
+    execute_process(COMMAND "${COCOGEN_DIR}/cocogen" "--backend" "${BACKEND}" "${DSL_FILE}"
         RESULT_VARIABLE COCOGEN_RET
         WORKING_DIRECTORY "${PROJECT_BINARY_DIR}"
         INPUT_FILE "${DSL_FILE}"
@@ -35,17 +35,10 @@ Maybe you forgot building the coconut project?")
     endif()
 endfunction()
 
-# IF people want control over interfaces of src and includes.
-macro(coconut_generate DSL_FILE)
-    cocogen_do_generate("${DSL_FILE}")
-    set(COCONUT_INCLUDE_DIRS ${CMAKE_BINARY_DIR}/generated/include/ ${COCONUT_ROOT_DIR}/copra/include/)
-    file(GLOB COCONUT_SOURCES ${CMAKE_BINARY_DIR}/generated/src/*.c ${COCONUT_ROOT_DIR}/copra/src/*.c)
-endmacro()
-
 function(coconut_target_generate TARGET DSL_FILE BACKEND)
     cocogen_do_generate("${DSL_FILE}" "${BACKEND}")
-    file(GLOB COCONUT_SOURCES ${CMAKE_BINARY_DIR}/generated/src/*.c ${COCONUT_ROOT_DIR}/copra/src/*.c)
+    file(GLOB COCONUT_SOURCES "${PROJECT_BINARY_DIR}/ccngen/*.c" "${COCONUT_ROOT_DIR}/copra/src/*.c")
     target_sources("${TARGET}" PRIVATE "${COCONUT_SOURCES}")
-    target_include_directories("${TARGET}" PRIVATE ${CMAKE_BINARY_DIR}/generated/include/ ${COCONUT_ROOT_DIR}/copra/include/)
+    target_include_directories("${TARGET}" PRIVATE "${PROJECT_BINARY_DIR}/ccngen/" "${COCONUT_ROOT_DIR}/copra/")
     target_link_libraries("${TARGET}" PRIVATE coconut::palm)
 endfunction()
