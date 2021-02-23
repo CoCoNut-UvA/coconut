d35abbb8eb2381f70a5f8eb40b4027d587af7477
DF <leegbestand@gmail.com>
2021-02-23 23:56:52 +0100

    Fix gen of enum inside trav_data.

--- cocogen/dynamic_backend/gen_trav_structs.c
+++ cocogen/dynamic_backend/gen_trav_structs.c
@@ -1,14 +1,17 @@
-
+#include "palm/dbug.h"
 #include "gen_helpers/out_macros.h"
 #include "ccn/dynamic_core.h"
 #include "gen_helpers/format.h"
 #include "globals.h"
+#include "frontend/symboltable.h"
 
 static node_st *ast;
+static node_st *ste;
 
 node_st *DGTDSast(node_st *node)
 {
     ast = node;
+    ste = AST_STABLE(ast);
     TRAVchildren(ast);
     return node;
 }
@@ -33,7 +36,13 @@ node_st *DGTDSitravdata(node_st *node)
     int indent = 0;
     if (ITRAVDATA_TYPE(node) == AT_link) {
         OUT_FIELD("node_st *%s", ID_ORIG(ITRAVDATA_NAME(node)));
-    } else if (ITRAVDATA_TYPE(node) == AT_user) {
+    } else if (ITRAVDATA_TYPE(node) == AT_link_or_enum) {
+        node_st *corr_enum = STlookup(ste, ITRAVDATA_TYPE_REFERENCE(node));
+        DBUG_ASSERT(corr_enum != NULL, "Oeps, invalid system.");
+        DBUG_ASSERT(NODE_TYPE(corr_enum) == NT_IENUM, "Travdata type invalid.");
+        OUT_FIELD("enum %s %s", ID_ORIG(IENUM_NAME(corr_enum)), ID_ORIG(ITRAVDATA_NAME(node)));
+    }
+    else if (ITRAVDATA_TYPE(node) == AT_user) {
         AST_USES_UNSAFE(ast) = true;
         OUT_FIELD("%s %s", ID_ORIG(ITRAVDATA_TYPE_REFERENCE(node)), ID_ORIG(ITRAVDATA_NAME(node)));
     } else {
