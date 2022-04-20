18727bc6c9a67e8d3e0e31c764f951068b507d95
dfrolich <dev@dnfh.nl>
2022-04-20 16:23:27 +0200

    Add documentation for system traversal.

--- cocogen/dynamic_backend/gen_system_traversals.c
+++ cocogen/dynamic_backend/gen_system_traversals.c
@@ -15,22 +15,32 @@ static char *prefix = NULL;
 static node_st *ast;
 static char *system_trav_table_template = "const ccn_trav_ft ccn_%s_vtable[_NT_SIZE] = { &TRAVerror, ";
 
-static void GenSystemTraversalVtable(node_st *node, char *pref, char *trav_name)
+/**
+ * Generates a vtable according to the given data.
+ * The name of the vtable is `ccn_<trav_name>_vtable` and should correspond
+ * to the one defined in vtable entries(see dynamic_backend/gen_trav_table.c).
+ * 
+ * The second parameter (trav_prefix) is the prefix or uid of the system traversal.
+ * This will be used in the generation of the handler functions for the traversal.
+ * It is a good idea to prefix this with `CCN_`.
+ * Keep this in mind when generating your system traversal handlers
+ */
+static void GenSystemTraversalVtable(char *trav_prefix, char *trav_name)
 {
     GeneratorContext *ctx = globals.gen_ctx;
     OUT(system_trav_table_template, trav_name);
-    prefix = pref;
-    TRAVopt(AST_INODES(node));
+    prefix = trav_prefix;
+    TRAVopt(AST_INODES(ast));
     OUT("};\n");
 }
 
 node_st *DGSTast(node_st *node)
 {
     ast = node;
-    GenSystemTraversalVtable(node, "CPY", "copy");
-    GenSystemTraversalVtable(node, "CHK", "check");
-    GenSystemTraversalVtable(node, "DEL", "free");
-    GenSystemTraversalVtable(node, "ERR", "error");
+    GenSystemTraversalVtable("CPY", "copy");
+    GenSystemTraversalVtable("CHK", "check");
+    GenSystemTraversalVtable("DEL", "free");
+    GenSystemTraversalVtable("ERR", "error");
 
     return node;
 }
